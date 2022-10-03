2b3a2f5d8ebc93790190085262c9b4a0d0141d2f
Everaldo Júnior <everaldo.junior98@live.com>
2022-10-03 20:05:14 -0300

    Finishing push button
    Updating circuit library;
    Finishing push button;

--- INO3D/Assets/Scripts/Components/PushButton.cs
+++ INO3D/Assets/Scripts/Components/PushButton.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using Assets.Scripts.Components.Base;
 using Assets.Scripts.Managers;
+using CircuitSharp.Components;
+using CircuitSharp.Core;
 using UnityEngine;
 
 namespace Assets.Scripts.Components
@@ -8,6 +11,8 @@ namespace Assets.Scripts.Components
     public class PushButton : InoComponent
     {
         #region Fields
+
+        private SwitchSPST switchSpst;
 
         private Animator animator;
 
@@ -20,12 +25,15 @@ namespace Assets.Scripts.Components
             if (SimulationManager.Instance.IsSimulating())
             {
                 animator.SetBool("Down", true);
+                switchSpst.Close();
             }
         }
 
         private void OnMouseUp()
         {
             animator.SetBool("Down", false);
+            if (SimulationManager.Instance.IsSimulating())
+                switchSpst.Open();
         }
 
         #endregion
@@ -34,26 +42,38 @@ namespace Assets.Scripts.Components
 
         public override void GenerateCircuitElement()
         {
+            if (IsConnected())
+            {
+                switchSpst = SimulationManager.Instance.CreateElement<SwitchSPST>();
+                switchSpst.Open();
 
+                LeadByPortName = new Dictionary<string, Lead>
+                {
+                    {"A", switchSpst.LeadA},
+                    {"C", switchSpst.LeadA},
+                    {"B", switchSpst.LeadB},
+                    {"D", switchSpst.LeadB}
+                };
+
+                foreach (var pair in ConnectedPorts)
+                    SimulationManager.Instance.Connect(GetLead(pair.Key), pair.Value.GetLead());
+            }
         }
 
         public override void OnSimulationTick()
         {
-
         }
 
         public override void DrawPropertiesWindow()
         {
-
         }
 
         protected override void SetupPorts()
         {
-            DebugPins = true;
             DefaultHeight = 0.18f;
             Pins.Add(Tuple.Create("A", new Vector3(-0.0737f, 0.0196f, 0.0516f)));
-            Pins.Add(Tuple.Create("B", new Vector3(0.073f, 0.0196f, 0.0516f)));
-            Pins.Add(Tuple.Create("C", new Vector3(-0.0737f, 0.0196f, -0.0516f)));
+            Pins.Add(Tuple.Create("C", new Vector3(0.073f, 0.0196f, 0.0516f)));
+            Pins.Add(Tuple.Create("B", new Vector3(-0.0737f, 0.0196f, -0.0516f)));
             Pins.Add(Tuple.Create("D", new Vector3(0.073f, 0.0196f, -0.0516f)));
 
             animator = GetComponent<Animator>();
@@ -63,7 +83,7 @@ namespace Assets.Scripts.Components
         {
             var saveFile = new PushButtonSaveFile
             {
-                PrefabName = "Led",
+                PrefabName = "PushButton",
 
                 PositionX = transform.position.x,
                 PositionY = transform.position.y,
